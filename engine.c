5c400a7aeeb88cdbc69944e6e7384c239c86ac16
VmdJAVem <unixlunatic@proton.me>
2026-02-26 14:38:17 -0500

    playing with a GUI

--- engine.c
+++ engine.c
@@ -288,10 +288,14 @@ void generateKingCaptures(moveLists * ml, color c, Tablero * t);
 bool testUnmakeMove(Tablero * original, Move move, color c);
 void testUnmakeAll();
 int main() {
+	setbuf(stdout, NULL); // Desactiva el buffering de stdout
+	setbuf(stdin, NULL);
 	initAttackTables();
+	initZobrist();
 	/*
 	testZobrist();
 	testTT();
+	testUnmakeAll();
 	*/
 	while (true) {
 		if (inputAvaliable()) {
@@ -1753,31 +1757,46 @@ bool inputAvaliable() {
 	return (ret > 0);
 }
 void proccesUCICommands(char command[256], Tablero * t) {
-	fflush(stdout);
-	if (strcmp(command, "quit\n") == 0) {
+	char * nl = strchr(command, '\n');
+	if (nl)
+		*nl = '\0';
+
+	if (strcmp(command, "quit") == 0) {
 		exit(0);
+	}
+	if (strcmp(command, "ping") == 0) {
+		printf("pong\n");
+		fflush(stdout);
 		return;
-	} else if (strcmp(command, "stop\n") == 0) {
+	}
+	if (strcmp(command, "stop") == 0) {
 		stopRequested = true;
 		return;
-	} else if (strcmp(command, "uci\n") == 0) {
+	}
+	if (strcmp(command, "uci") == 0) {
 		printf("id name Supplycat\n");
 		printf("id author  arkar\n");
 		printf("uciok\n");
+		fflush(stdout);
 		return;
-	} else if (strcmp(command, "isready\n") == 0) {
+	}
+	if (strcmp(command, "isready") == 0) {
 		printf("readyok\n");
+		fflush(stdout);
 		return;
-	} else if (strcmp(command, "ucinewgame") == 0) {
+	}
+	if (strcmp(command, "ucinewgame") == 0) {
 		memset(tt, 0, sizeof(tt));
 		memset(history, 0, sizeof(history));
 		memset(killerMoves, 0, sizeof(killerMoves));
+		return;
 	}
-	if (debug) {
-		printf("DEBUG: full command = '%s'\n", command);
-		fflush(stdout);
-	}
+
+	// Comandos que pueden tener parámetros – usamos strtok
 	char * firstCommand = strtok(command, " \t\n\r\f\v");
+	if (!firstCommand)
+		return; // línea vacía
+
 	if (debug) {
 		printf("DEBUG: firstCommand = '%s'\n", firstCommand);
 		fflush(stdout);
@@ -1785,153 +1804,41 @@ void proccesUCICommands(char command[256], Tablero * t) {
 
 	if (strcmp(firstCommand, "debug") == 0) {
 		char * x = strtok(NULL, " \t\n\r\f\v");
-		if (strcmp(x, "off") == 0) {
-			printf("ENDING DEBUGING\n");
+		if (x && strcmp(x, "off") == 0) {
 			debug = false;
-		} else if (strcmp(x, "on") == 0) {
+			printf("Debug off\n");
+		} else if (x && strcmp(x, "on") == 0) {
 			debug = true;
-			printf("DEBUGING\n");
+			printf("Debug on\n");
 		}
+		fflush(stdout);
+		return;
 	}
+
 	if (strcmp(firstCommand, "position") == 0) {
-		if (debug) {
-			printBitboard(t->allOccupiedSquares);
-		}
 		char * secondCommand = strtok(NULL, " \t\n\r\f\v");
+		if (!secondCommand)
+			return;
 		if (debug) {
 			printf("DEBUG: secondCommand = '%s'\n", secondCommand);
+			fflush(stdout);
 		}
+
 		if (strcmp(secondCommand, "startpos") == 0) {
 			initBoard(t);
-			if (debug) {
-				printf("DEBUG: after initBoard, white pawns = "
-				       "%lx\n",
-				       t->piezas[blancas][peon]);
-			}
 			colorToMove = blancas;
 			char * movesToken = strtok(NULL, " ");
-			if (movesToken != NULL && strcmp(movesToken, "moves") == 0) {
+			if (movesToken && strcmp(movesToken, "moves") == 0) {
 				char * move = strtok(NULL, " ");
-				while (move != NULL) {
-					casilla to = (stringToSq(move + 2));
-					casilla from = (stringToSq(move));
+				while (move) {
+					// Parseo del movimiento (igual que tenías)
+					casilla to = stringToSq(move + 2);
+					casilla from = stringToSq(move);
 					tipoDePieza promo = 0;
 					tipoDePieza piece = -1;
 					int special = 0;
-					tipoDePieza capture = 0;
-					for (int i = peon; i <= rey; i++) {
-						if (BB_SQUARE(from) & t->piezas[colorToMove][i]) {
-							piece = i;
-							break;
-						}
-					}
-					if (piece == -1) {
-						printf("ERROR: INVALID MOVE\n");
-						exit(256);
-					}
-					if (move[4] != '\0') {
-						promo = charToPiece(move[4]);
-						special = 3;
-					}
-					if (BB_SQUARE(to) & t->allPieces[!colorToMove]) {
-						for (int p = peon; p <= rey; p++) {
-							if (t->piezas[!colorToMove][p] & BB_SQUARE(to)) {
-								capture = p;
-								break;
-							}
-						}
-					}
-					if (piece == peon && to == t->enPassantSquare) {
-						special = 1;
-					}
-					if (piece == rey) {
-						if ((colorToMove == blancas && from == e1 && (to == g1 || to == c1)) ||
-						    (colorToMove == negras && from == e8 && (to == g8 || to == c8))) {
-							special = 2;
-						}
-					}
-					Move newMove = {from, to, piece, capture, special, promo};
-					makeMove(&newMove, t, colorToMove);
-					move = strtok(NULL, " ");
-					colorToMove = !colorToMove;
-					updateBoardCache(t);
-				}
-			}
-			if (debug) {
-				printBitboard(t->allOccupiedSquares);
-			}
-		} else if (strcmp(secondCommand, "fen") == 0) {
-			memset(t, 0, sizeof(Tablero));
-			char * fenString = strtok(NULL, " \t\n\r\f\v");
-			if (debug) {
-				printf("DEBUG: fenString = '%s'\n", fenString);
-			}
-			int rank = 7;
-			int file = 0;
-			for (int i = 0; fenString[i] != '\0'; i++) {
-				char currentChar = fenString[i];
-				if (currentChar == '/') {
-					rank--;
-					file = 0;
-					continue;
-				}
-				if (isdigit(currentChar)) {
-					file += currentChar - '0';
-					if (file > 7) {
-						file = 7;
-					}
-					continue;
-				} else {
-					color c = isupper(currentChar) ? blancas : negras;
-					tipoDePieza piece = charToPiece(currentChar);
-					casilla square = rank * 8 + file;
-					t->piezas[c][piece] |= BB_SQUARE(square);
-					file++;
-					if (debug) {
-						printf("Placing %c at square %d\n", currentChar, square);
-					}
-				}
-			}
-			updateBoardCache(t);
-			char * activeColor = strtok(NULL, " ");
-			colorToMove = strcmp(activeColor, "w") ? negras : blancas;
-			char * castlingRights = strtok(NULL, " ");
-			if (strchr(castlingRights, '-') != NULL) {
-				t->castlingRights = 0;
-			}
-			if (strchr(castlingRights, 'K') != NULL) {
-				t->castlingRights |= WHITE_OO;
-			}
-			if (strchr(castlingRights, 'Q') != NULL) {
-				t->castlingRights |= WHITE_OOO;
-			}
-			if (strchr(castlingRights, 'k') != NULL) {
-				t->castlingRights |= BLACK_OO;
-			}
-			if (strchr(castlingRights, 'q') != NULL) {
-				t->castlingRights |= BLACK_OOO;
-			}
-			char * enPassantSquare = strtok(NULL, " ");
-			if (strchr(enPassantSquare, '-')) {
-				t->enPassantSquare = -1;
-			} else {
-				casilla sq = stringToSq(enPassantSquare);
-				t->enPassantSquare = sq;
-			}
-			char * halfmoveClock = strtok(NULL, " ");
-			t->halfmoveClock = atoi(halfmoveClock);
-			char * fullMove = strtok(NULL, " ");
-			t->fullMoves = atoi(fullMove);
-			char * movesToken = strtok(NULL, " ");
-			if (movesToken != NULL && strcmp(movesToken, "moves") == 0) {
-				char * move = strtok(NULL, " ");
-				while (move != NULL) {
-					casilla to = (stringToSq(move + 2));
-					casilla from = (stringToSq(move));
-					tipoDePieza promo = 0;
-					tipoDePieza piece = 0;
-					int special = 0;
 					tipoDePieza capture = -1;
+
 					for (int i = peon; i <= rey; i++) {
 						if (BB_SQUARE(from) & t->piezas[colorToMove][i]) {
 							piece = i;
@@ -1939,7 +1846,7 @@ void proccesUCICommands(char command[256], Tablero * t) {
 						}
 					}
 					if (piece == -1) {
-						printf("ERROR: INVALID MOVE\n");
+						fprintf(stderr, "ERROR: INVALID MOVE\n");
 						exit(256);
 					}
 					if (move[4] != '\0') {
@@ -1970,107 +1877,193 @@ void proccesUCICommands(char command[256], Tablero * t) {
 					colorToMove = !colorToMove;
 				}
 			}
-
-			t->hash = computeZobrist(&zobrist, t, colorToMove);
+			if (debug)
+				printBitboard(t->allOccupiedSquares);
+			return;
 		}
-	} else if (strcmp(firstCommand, "go") == 0 || strcmp(firstCommand, "go\n") == 0) {
-		parameters = (goParameters){
-		    .wtime = -1,
-		    .btime = -1,
-		    .winc = -1,
-		    .binc = -1,
-		    .movestogo = -1,
-		    .depth = -1,
-		    .movetime = -1,
-		    .infinite = false,
-		};
-		char * secondCommand = strtok(NULL, " \t\n\r\f\v");
-		char possible2ndCommands[8][10] = {"wtime",	"btime", "winc",     "binc",
-						   "movestogo", "depth", "movetime", "infinite"};
-		if (secondCommand == NULL) {
-			if (debug) {
-				printf("DEBUG: before negaMax, white pawns = "
-				       "%lx\n",
-				       t->piezas[blancas][peon]);
-			}
-			moveScore bestMove = negaMax(t, colorToMove, -1);
-			printf("bestmove %s\n", moveToStr(&bestMove.move));
-		} else {
-			while (secondCommand != NULL) {
-				for (int i = 0; i <= 7; i++) {
-					if (strcmp(possible2ndCommands[i], secondCommand) == 0) {
-						char * valueStr = strtok(NULL, " \t\n\r\f\v");
-						switch (i) {
-							case 0:
-								parameters.wtime = atoi(valueStr);
-								break;
-							case 1:
-								parameters.btime = atoi(valueStr);
-								break;
-							case 2:
-								parameters.winc = atoi(valueStr);
-								break;
-							case 3:
-								parameters.binc = atoi(valueStr);
-								break;
-							case 4:
-								parameters.movestogo = atoi(valueStr);
-								break;
-							case 5:
 
-								parameters.depth = atoi(valueStr);
-								break;
-							case 6:
-								parameters.movetime = atoi(valueStr);
-								break;
-							case 7:
-								parameters.infinite = true;
-								break;
+		if (strcmp(secondCommand, "fen") == 0) {
+			memset(t, 0, sizeof(Tablero));
+			char * fenString = strtok(NULL, " \t\n\r\f\v");
+			if (!fenString)
+				return;
+			if (debug)
+				printf("DEBUG: fenString = '%s'\n", fenString);
+			int rank = 7;
+			int file = 0;
+			for (int i = 0; fenString[i]; i++) {
+				char currentChar = fenString[i];
+				if (currentChar == '/') {
+					rank--;
+					file = 0;
+					continue;
+				}
+				if (isdigit(currentChar)) {
+					file += currentChar - '0';
+					continue;
+				}
+				color c = isupper(currentChar) ? blancas : negras;
+				tipoDePieza piece = charToPiece(currentChar);
+				casilla square = rank * 8 + file;
+				t->piezas[c][piece] |= BB_SQUARE(square);
+				file++;
+				if (debug)
+					printf("Placing %c at square %d\n", currentChar, square);
+			}
+			updateBoardCache(t);
+			char * activeColor = strtok(NULL, " ");
+			colorToMove = strcmp(activeColor, "w") ? negras : blancas;
+			char * castlingRights = strtok(NULL, " ");
+			t->castlingRights = 0;
+			if (strchr(castlingRights, 'K'))
+				t->castlingRights |= WHITE_OO;
+			if (strchr(castlingRights, 'Q'))
+				t->castlingRights |= WHITE_OOO;
+			if (strchr(castlingRights, 'k'))
+				t->castlingRights |= BLACK_OO;
+			if (strchr(castlingRights, 'q'))
+				t->castlingRights |= BLACK_OOO;
+			char * enPassantSquare = strtok(NULL, " ");
+			if (strchr(enPassantSquare, '-')) {
+				t->enPassantSquare = -1;
+			} else {
+				t->enPassantSquare = stringToSq(enPassantSquare);
+			}
+			char * halfmoveClock = strtok(NULL, " ");
+			t->halfmoveClock = atoi(halfmoveClock);
+			char * fullMove = strtok(NULL, " ");
+			t->fullMoves = atoi(fullMove);
+			char * movesToken = strtok(NULL, " ");
+			if (movesToken && strcmp(movesToken, "moves") == 0) {
+				char * move = strtok(NULL, " ");
+				while (move) {
+					// (mismo parseo que en startpos)
+					casilla to = stringToSq(move + 2);
+					casilla from = stringToSq(move);
+					tipoDePieza promo = 0;
+					tipoDePieza piece = -1;
+					int special = 0;
+					tipoDePieza capture = -1;
+
+					for (int i = peon; i <= rey; i++) {
+						if (BB_SQUARE(from) & t->piezas[colorToMove][i]) {
+							piece = i;
+							break;
 						}
 					}
-				}
-				secondCommand = strtok(NULL, " \t\n\r\f\v");
-			}
-			if (parameters.depth != -1) {
-				clock_t start, end;
-				double cpu_time_used;
-				start = clock();
-				moveScore bestMove = negaMaxFixedDepth(t, colorToMove, parameters.depth);
-				end = clock();
-				cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-				printf("Time taken by myFunction: %f seconds\n", cpu_time_used);
-				printf("bestmove %s\n", moveToStr(&bestMove.move));
-
-			} else if (parameters.movetime != -1) {
-				moveScore bestMove = negaMax(t, colorToMove, parameters.movetime);
-				printf("bestmove %s\n", moveToStr(&bestMove.move));
-			} else if (parameters.infinite) {
-				moveScore bestMove = negaMax(t, colorToMove, -1);
-				printf("bestmove %s\n", moveToStr(&bestMove.move));
-			} else if (parameters.wtime != -1 && parameters.btime != -1) {
-				int clockTime = 0;
-				int increment = 0;
-				if (colorToMove == blancas) {
-					clockTime = parameters.wtime;
-					if (parameters.winc != -1) {
-						increment = parameters.winc;
+					if (piece == -1) {
+						fprintf(stderr, "ERROR: INVALID MOVE\n");
+						exit(256);
 					}
-				} else if (colorToMove == negras) {
-					clockTime = parameters.btime;
-					if (parameters.binc != -1) {
-						increment = parameters.binc;
+					if (move[4] != '\0') {
+						promo = charToPiece(move[4]);
+						special = 3;
 					}
+					if (BB_SQUARE(to) & t->allPieces[!colorToMove]) {
+						for (int p = peon; p <= rey; p++) {
+							if (t->piezas[!colorToMove][p] & BB_SQUARE(to)) {
+								capture = p;
+								break;
+							}
+						}
+					}
+					if (piece == peon && to == t->enPassantSquare) {
+						special = 1;
+						capture = peon;
+					}
+					if (piece == rey) {
+						if ((colorToMove == blancas && from == e1 && (to == g1 || to == c1)) ||
+						    (colorToMove == negras && from == e8 && (to == g8 || to == c8))) {
+							special = 2;
+						}
+					}
+					Move newMove = {from, to, piece, capture, special, promo};
+					makeMove(&newMove, t, colorToMove);
+					move = strtok(NULL, " ");
+					colorToMove = !colorToMove;
 				}
-				int timeForThisMove =
-				    clockTime / (parameters.movestogo == -1 ? parameters.movestogo : 40) + increment;
-				timeForThisMove -= ((increment / timeForThisMove) * 100);
-				if (timeForThisMove < 100) {
-					timeForThisMove = 100;
-				}
-				moveScore bestMove = negaMax(t, colorToMove, timeForThisMove);
-				printf("bestmove %s\n", moveToStr(&bestMove.move));
 			}
+			t->hash = computeZobrist(&zobrist, t, colorToMove);
+			return;
 		}
+	}
+
+	if (strcmp(firstCommand, "go") == 0) {
+		// Reiniciamos parámetros
+		parameters = (goParameters){.wtime = -1,
+					    .btime = -1,
+					    .winc = -1,
+					    .binc = -1,
+					    .movestogo = -1,
+					    .depth = -1,
+					    .movetime = -1,
+					    .infinite = false};
+		char * token = strtok(NULL, " \t\n\r\f\v");
+		char * possible2ndCommands[8] = {"wtime",     "btime", "winc",	   "binc",
+						 "movestogo", "depth", "movetime", "infinite"};
+
+		while (token) {
+			for (int i = 0; i < 8; i++) {
+				if (strcmp(token, possible2ndCommands[i]) == 0) {
+					if (i == 7) { // infinite
+						parameters.infinite = true;
+						break;
+					}
+					char * valueStr = strtok(NULL, " \t\n\r\f\v");
+					if (!valueStr)
+						break;
+					int val = atoi(valueStr);
+					switch (i) {
+						case 0:
+							parameters.wtime = val;
+							break;
+						case 1:
+							parameters.btime = val;
+							break;
+						case 2:
+							parameters.winc = val;
+							break;
+						case 3:
+							parameters.binc = val;
+							break;
+						case 4:
+							parameters.movestogo = val;
+							break;
+						case 5:
+							parameters.depth = val;
+							break;
+						case 6:
+							parameters.movetime = val;
+							break;
+					}
+					break;
+				}
+			}
+			token = strtok(NULL, " \t\n\r\f\v");
+		}
+
+		moveScore bestMove;
+		if (parameters.depth != -1) {
+			bestMove = negaMaxFixedDepth(t, colorToMove, parameters.depth);
+		} else if (parameters.movetime != -1) {
+			bestMove = negaMax(t, colorToMove, parameters.movetime);
+		} else if (parameters.infinite) {
+			bestMove = negaMax(t, colorToMove, -1);
+		} else if (parameters.wtime != -1 && parameters.btime != -1) {
+			int clockTime = (colorToMove == blancas) ? parameters.wtime : parameters.btime;
+			int inc = (colorToMove == blancas) ? parameters.winc : parameters.binc;
+			int movesLeft = (parameters.movestogo > 0) ? parameters.movestogo : 40;
+			int timeForMove = clockTime / movesLeft + inc;
+			if (timeForMove < 100)
+				timeForMove = 100;
+			bestMove = negaMax(t, colorToMove, timeForMove);
+		} else {
+			// Si no hay parámetros, go = infinito
+			bestMove = negaMax(t, colorToMove, -1);
+		}
+		printf("bestmove %s\n", moveToStr(&bestMove.move));
+		fflush(stdout);
+		return;
 	}
 }
 static inline tipoDePieza charToPiece(char c) {
